f72f11aba01174f3ab013c4cf0dad5c94fb6e537
Trevor <jtrevag@gmail.com>
2017-09-15 14:24:26 -0500

    added files once more

---
+++ src/components/search_bar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// Functional component: literally a function. Something goes in, something comes out.
+const SearchBar = () => {
+  return <input />; // Turns into React.createElement()
+};
+
+export default SearchBar;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBt7Z4lOiUuv0ZP4wtFHU6ZNRwZZI9tSUY';
+
+// Create a new component. This comonent shoudl produce
+// some HTML
+const App = () => {
+  return (
+    <div>
+      <SearchBar />;
+    </div>
+  );
+}
+
+// Take this component's generated HTML and put it
+// on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));

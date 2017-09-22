f95bb056531dcae33d0699dee9bfd44c3c84a95f
Trevor <jtrevag@gmail.com>
2017-09-22 14:32:01 -0500

    finish lecture 21

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar_class';
 
 const API_KEY = 'AIzaSyBt7Z4lOiUuv0ZP4wtFHU6ZNRwZZI9tSUY';
 
+YTSearch({key: API_KEY, term:'surfboards'}, function(data) {
+  console.log(data);
+});
+// The most parent component should be resposible for getting data for the app.
 // Create a new component. This comonent shoudl produce
 // some HTML
 const App = () => {

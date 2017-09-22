d6aaec724f819380250aa66474f7c64a46dcb273
Trevor <jtrevag@gmail.com>
2017-09-22 14:41:15 -0500

    finish lecture 22

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar_class';
-
 const API_KEY = 'AIzaSyBt7Z4lOiUuv0ZP4wtFHU6ZNRwZZI9tSUY';
 
-YTSearch({key: API_KEY, term:'surfboards'}, function(data) {
-  console.log(data);
-});
+
+
 // The most parent component should be resposible for getting data for the app.
 // Create a new component. This comonent shoudl produce
 // some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos : [] };
+
+    YTSearch({key: API_KEY, term:'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
-
 ReactDOM.render(<App />, document.querySelector('.container'));

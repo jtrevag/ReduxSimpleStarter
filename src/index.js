e5777edc586a2c136c8de06bbfa9f953d3d6b061
Trevor <jtrevag@gmail.com>
2017-09-29 16:06:15 -0500

    finish lecture 29

--- src/index.js
+++ src/index.js
@@ -15,10 +15,15 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos : [] };
+    this.state = {
+      videos : [],
+      selectedVideo: null };
 
     YTSearch({key: API_KEY, term:'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -26,8 +31,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
       </div>
     );
   }

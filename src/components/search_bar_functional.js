03d81ace0f81f4736f74ba4da15bc44843ec726a
jtrevag <jtrevag@gmail.com>
2017-10-13 14:22:46 -0500

    initial push

--- src/components/search_bar_functional.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// Functional component: literally a function. Something goes in, something comes out.
-const SearchBarFunctional = () => {
-  return <input />; // Turns into React.createElement()
-};
-
-export default SearchBarFunctional;
